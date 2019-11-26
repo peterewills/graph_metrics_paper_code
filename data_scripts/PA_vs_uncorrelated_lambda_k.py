54b5c9dcd6fe22cdc48e12e9408f660be9cc4d48
Peter Wills <peter.e.wills@gmail.com>
2019-11-26 14:38:32 -0800

    run experiments on larger graphs

--- data_scripts/PA_vs_uncorrelated_lambda_k.py
+++ data_scripts/PA_vs_uncorrelated_lambda_k.py
@@ -1,9 +1,10 @@
 # Same as PA_vs_uncorrelated.py, but for lamnda_k distances
 
 import netcomp as nc
+
 data_dir = "../pickled_data"
 
-from joblib import Parallel, delayed  
+from joblib import Parallel, delayed
 import multiprocessing
 import os
 import networkx as nx
@@ -11,6 +12,7 @@ import pandas as pd
 import time
 import pickle
 import itertools as it
+from tqdm import tqdm
 
 ####################################
 ### SET PARAMETERS
@@ -20,85 +22,104 @@ num_cores = multiprocessing.cpu_count()
 # size of ensemble
 ensemble_len = 500
 
-n = 100
+n = 1000
 m = 6
-p = (n-m)*m / (n*(n-1)/2) # so that volume of ER and BA graphs match
+p = (n - m) * m / (n * (n - 1) / 2)  # so that volume of ER and BA graphs match
 
 
 ####################################
 ## DEFINE IMPORTANT FUNCTIONS
 ####################################
 
-def distance(dist_func,A,B): return dist_func(A,B)
 
-k_list = [1,2,5,10,20,50,75,90,99]
+def distance(dist_func, A, B):
+    return dist_func(A, B)
 
-def flatten(l): return [item for sublist in l for item in sublist]
+
+k_list = [1, 2, 10, 100, 300, 999]
+
+
+def flatten(l):
+    return [item for sublist in l for item in sublist]
+
 
 distances = []
 labels = []
 
+
 def return_lambdas(k):
-    return [lambda A1,A2: nc.lambda_dist(A1,A2,kind='adjacency',k=k),
-            lambda A1,A2: nc.lambda_dist(A1,A2,kind='laplacian',k=k),
-            lambda A1,A2: nc.lambda_dist(A1,A2,kind='laplacian_norm',k=k)]
+    return [
+        lambda A1, A2: nc.lambda_dist(A1, A2, kind="adjacency", k=k),
+        lambda A1, A2: nc.lambda_dist(A1, A2, kind="laplacian", k=k),
+        lambda A1, A2: nc.lambda_dist(A1, A2, kind="laplacian_norm", k=k),
+    ]
+
 
 # can't make this work without using map
-distances = list(map(return_lambdas,k_list))
+distances = list(map(return_lambdas, k_list))
 distances = flatten(distances)
 
 for k in k_list:
 
-    labels_k = ['Lambda (Adjacency, k={})'.format(k),
-                'Lambda (Laplacian, k={})'.format(k),
-                'Lambda (Normalized Laplacian, k={})'.format(k)]
+    labels_k = [
+        "Lambda (Adjacency, k={})".format(k),
+        "Lambda (Laplacian, k={})".format(k),
+        "Lambda (Normalized Laplacian, k={})".format(k),
+    ]
 
     labels += labels_k
 
-    
-def grab_data(i,null=True):
 
-    if i % 100 == 0 : print('Iteration {}.'.format(i))
-    
-    G1 = nx.erdos_renyi_graph(n,p)
+def grab_data(i, null=True):
+
+    G1 = nx.erdos_renyi_graph(n, p)
     if null:
-        G2 = nx.erdos_renyi_graph(n,p)
+        G2 = nx.erdos_renyi_graph(n, p)
     else:
-        G2 = nx.barabasi_albert_graph(n,m)
-    A1,A2 = [nx.adjacency_matrix(G).todense() for G in [G1,G2]]
-    
-    adj_distances = pd.Series([distance(dfun,A1,A2) for dfun in distances],
-                              index = labels, name = 'Adjacency Distances')
-    
-    data = pd.concat([adj_distances],axis=1)
-    
+        G2 = nx.barabasi_albert_graph(n, m)
+    A1, A2 = [nx.adjacency_matrix(G).todense() for G in [G1, G2]]
+
+    adj_distances = pd.Series(
+        [distance(dfun, A1, A2) for dfun in distances],
+        index=labels,
+        name="Adjacency Distances",
+    )
+
+    data = pd.concat([adj_distances], axis=1)
+
     return data
 
 
 ####################################
-## TAKE DATA 
+## TAKE DATA
 ####################################
 
-print('Running on {} cores.'.format(num_cores))
-print('ER/BA Lambda K Distance Comparison.')
+print("Running on {} cores.".format(num_cores))
+
+print("ER/BA Lambda K Distance Comparison.")
+
+if not os.path.exists(data_dir):
+    raise IOError(f"Data directory {data_dir} does not exist!")
 
 start = time.time()
-results_null = Parallel(n_jobs=num_cores)(delayed(grab_data)(i)
-                                          for i in range(ensemble_len))
+results_null = Parallel(n_jobs=num_cores)(
+    delayed(grab_data)(i) for i in tqdm(range(ensemble_len))
+)
 end = time.time()
 
-print('Null data complete. Total time elapsed: {} seconds.'.format(end-start))
+print("Null data complete. Total time elapsed: {} seconds.".format(end - start))
 
-results_df_null = pd.concat(results_null,axis=1)
+results_df_null = pd.concat(results_null, axis=1)
 
 start = time.time()
-results_not_null = Parallel(n_jobs=num_cores)(delayed(grab_data)(i,null=False)
-                                              for i in range(ensemble_len))
+results_not_null = Parallel(n_jobs=num_cores)(
+    delayed(grab_data)(i, null=False) for i in tqdm(range(ensemble_len))
+)
 end = time.time()
 
-print('Alternative data complete. Total time elapsed: {} seconds.'.format(end-start))
+print("Alternative data complete. Total time elapsed: {} seconds.".format(end - start))
 
-results_df_not_null = pd.concat(results_not_null,axis=1)
+results_df_not_null = pd.concat(results_not_null, axis=1)
 
 
 ####################################
@@ -106,26 +127,28 @@ results_df_not_null = pd.concat(results_not_null,axis=1)
 ####################################
 
 
-data_dict = {'n' : n,
-             'p' : p,
-             'm' : m,
-             'results_df_null' : results_df_null,
-             'results_df_not_null' : results_df_not_null,
-             'description' : """Comparison of distances between two ER graphs,
+data_dict = {
+    "n": n,
+    "p": p,
+    "m": m,
+    "results_df_null": results_df_null,
+    "results_df_not_null": results_df_not_null,
+    "description": """Comparison of distances between two ER graphs,
              and an ER and BA graph. Uses given parameters, and calculates for a
              variety of distances. Adjacency distance is the distances between
              the true adjacency matrices.
 
              This data only compares lambda distances, using different numbers
-             of eigenvalues."""}
+             of eigenvalues.""",
+}
 
 # Ensure we don't inadvertently overwrite an extant file
-file_name = 'PA_vs_uncorr_lambdak.p'
-path = os.path.join(data_dir,file_name)
+file_name = "PA_vs_uncorr_lambdak.p"
+path = os.path.join(data_dir, file_name)
 tag = 1
 while os.path.isfile(path):
-    file_name = 'PA_vs_uncorr_lambdak_{:02d}.p'.format(tag)
-    path = os.path.join(data_dir,file_name)
+    file_name = "PA_vs_uncorr_lambdak_{:02d}.p".format(tag)
+    path = os.path.join(data_dir, file_name)
     tag += 1
 
-pickle.dump(data_dict,open(os.path.join(data_dir,file_name),'wb'))
+pickle.dump(data_dict, open(os.path.join(data_dir, file_name), "wb"))
--- data_scripts/lattice_vs_rand_deg_seq.py
+++ data_scripts/lattice_vs_rand_deg_seq.py
@@ -2,7 +2,7 @@
 # with same deg. sequence as lattice.
 
 import netcomp as nc
-from joblib import Parallel, delayed  
+from joblib import Parallel, delayed
 import multiprocessing
 import os
 import networkx as nx
@@ -10,6 +10,7 @@ import pandas as pd
 import time
 import pickle
 import numpy as np
+from tqdm import tqdm
 
 ####################################
 ### SET PARAMETERS
@@ -21,11 +22,11 @@ num_cores = multiprocessing.cpu_count()
 # size of ensemble
 ensemble_len = 500
 
-n = 100
-N = 25
-M = 4
+n = 1000
+N = 100
+M = 10\
 
-G = nx.grid_2d_graph(N,M)
+G = nx.grid_2d_graph(N, M)
 deg_seq = [item[1] for item in G.degree_iter()]
 
 ####################################
@@ -33,65 +34,82 @@ deg_seq = [item[1] for item in G.degree_iter()]
 ####################################
 
 
-def distance(dist_func,A,B): return dist_func(A,B)
-
-lambda_adj = lambda A1,A2: nc.lambda_dist(A1,A2,kind='adjacency')
-lambda_lap = lambda A1,A2: nc.lambda_dist(A1,A2,kind='laplacian')
-lambda_nlap = lambda A1,A2: nc.lambda_dist(A1,A2,kind='laplacian_norm')
-res_dist = lambda A1,A2: nc.resistance_distance(A1,A2,check_connected=False)
-
-distances = [nc.edit_distance,res_dist,nc.deltacon0,nc.netsimile,
-            lambda_adj,lambda_lap,lambda_nlap]
-labels = ['Edit','Resistance Dist.','DeltaCon','NetSimile',
-          'Lambda (Adjacency)','Lambda (Laplacian)','Lambda (Normalized'
-          ' Laplacian)']
+def distance(dist_func, A, B):
+    return dist_func(A, B)
 
 
-def grab_data(i,null=True):
+lambda_adj = lambda A1, A2: nc.lambda_dist(A1, A2, kind="adjacency")
+lambda_lap = lambda A1, A2: nc.lambda_dist(A1, A2, kind="laplacian")
+lambda_nlap = lambda A1, A2: nc.lambda_dist(A1, A2, kind="laplacian_norm")
+res_dist = lambda A1, A2: nc.resistance_distance(A1, A2, check_connected=False)
 
-    
-    if i % 100 == 0 : print('Lattice Distance Comparison, Iteration {}.'.format(i))
-    
+distances = [
+    nc.edit_distance,
+    res_dist,
+    nc.deltacon0,
+    nc.netsimile,
+    lambda_adj,
+    lambda_lap,
+    lambda_nlap,
+]
+labels = [
+    "Edit",
+    "Resistance Dist.",
+    "DeltaCon",
+    "NetSimile",
+    "Lambda (Adjacency)",
+    "Lambda (Laplacian)",
+    "Lambda (Normalized" " Laplacian)",
+]
+
+
+def grab_data(i, null=True):
+
     G1 = nx.random_degree_sequence_graph(deg_seq)
     if null:
         G2 = nx.random_degree_sequence_graph(deg_seq)
     else:
-        G2 = nx.grid_2d_graph(N,M)
-    
-    A1,A2 = [nx.adjacency_matrix(G).todense() for G in [G1,G2]]
-    
-    adj_distances = pd.Series([distance(dfun,A1,A2) for dfun in distances],
-                              index = labels, name = 'Adjacency Distances')
-    
-    data = pd.concat([adj_distances],axis=1)
-    
+        G2 = nx.grid_2d_graph(N, M)
+
+    A1, A2 = [nx.adjacency_matrix(G).todense() for G in [G1, G2]]
+
+    adj_distances = pd.Series(
+        [distance(dfun, A1, A2) for dfun in distances],
+        index=labels,
+        name="Adjacency Distances",
+    )
+
+    data = pd.concat([adj_distances], axis=1)
+
     return data
 
 
 ####################################
-## TAKE DATA 
+## TAKE DATA
 ####################################
 
-print('Running on {} cores.'.format(num_cores))
-print('Lattice Distance Comparison.')
+print("Running on {} cores.".format(num_cores))
+print("Lattice Distance Comparison.")
 
 start = time.time()
-results_null = Parallel(n_jobs=num_cores)(delayed(grab_data)(i)
-                                          for i in range(ensemble_len))
+results_null = Parallel(n_jobs=num_cores)(
+    delayed(grab_data)(i) for i in tqdm(range(ensemble_len))
+)
 end = time.time()
 
-print('Null data complete. Total time elapsed: {} seconds.'.format(end-start))
+print("Null data complete. Total time elapsed: {} seconds.".format(end - start))
 
-results_df_null = pd.concat(results_null,axis=1)
+results_df_null = pd.concat(results_null, axis=1)
 
 start = time.time()
-results_not_null = Parallel(n_jobs=num_cores)(delayed(grab_data)(i,null=False)
-                                              for i in range(ensemble_len))
+results_not_null = Parallel(n_jobs=num_cores)(
+    delayed(grab_data)(i, null=False) for i in tqdm(range(ensemble_len))
+)
 end = time.time()
 
-print('Alternative data complete. Total time elapsed: {} seconds.'.format(end-start))
+print("Alternative data complete. Total time elapsed: {} seconds.".format(end - start))
 
-results_df_not_null = pd.concat(results_not_null,axis=1)
+results_df_not_null = pd.concat(results_not_null, axis=1)
 
 
 ####################################
@@ -99,22 +117,24 @@ results_df_not_null = pd.concat(results_not_null,axis=1)
 ####################################
 
 
-data_dict = {'n' : n,
-             'N' : 10,
-             'M' : 10,
-             'results_df_null' : results_df_null,
-             'results_df_not_null' : results_df_not_null,
-             'description' : """Comparison of distances between two random
+data_dict = {
+    "n": n,
+    "N": 10,
+    "M": 10,
+    "results_df_null": results_df_null,
+    "results_df_not_null": results_df_not_null,
+    "description": """Comparison of distances between two random
              graphs with given degree sequence, and a grid graph, with same
-             degree sequence."""}
+             degree sequence.""",
+}
 
 # Ensure we don't inadvertently overwrite an extant file
-file_name = 'lattice_vs_randDS_{}x{}.p'.format(N,M)
-path = os.path.join(data_dir,file_name)
+file_name = "lattice_vs_randDS_{}x{}.p".format(N, M)
+path = os.path.join(data_dir, file_name)
 tag = 1
 while os.path.isfile(path):
-    file_name = 'lattice_vs_randDS_{}x{}_{:02d}.p'.format(N,M,tag)
-    path = os.path.join(data_dir,file_name)
+    file_name = "lattice_vs_randDS_{}x{}_{:02d}.p".format(N, M, tag)
+    path = os.path.join(data_dir, file_name)
     tag += 1
 
-pickle.dump(data_dict,open(os.path.join(data_dir,file_name),'wb'))
+pickle.dump(data_dict, open(os.path.join(data_dir, file_name), "wb"))
